1be633966d92edbd9c6625d3ffa40376d0e8b895
cheruden <cheruden25@gmail.com>
2021-05-22 13:39:07 +0300

    initial add of github component

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,44 @@
+
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+  profile: any;
+  repos: any;
+  username: string;
+
+  constructor(private githubService: GithubService) {
+    this.githubService.updateUserProfile(this.username);
+    this.githubService.getProfileInformation().subscribe(profile => {
+      this.profile = profile;
+    });
+
+    this.githubService.getProfileRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+
+  }
+  getUserProfile() {
+    this.githubService.updateUserProfile(this.username);
+    this.githubService.getProfileInformation().subscribe(profile => {
+      this.profile = profile;
+    });
+
+    this.githubService.getProfileRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+  }
+
+  ngOnInit() {
+    this.githubService.updateUserProfile('cheruden');
+    this.githubService.getProfileInformation().subscribe(profile => this.profile = profile);
+    // this.githubService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+
+  }
+}
