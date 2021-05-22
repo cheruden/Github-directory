fb22684d3e44c0fc759d9d95acd02f5f5d72dd2d
cheruden <cheruden25@gmail.com>
2021-05-22 13:35:09 +0300

    initial add

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  private username: string;
+  constructor(private http: HttpClient) {
+    this.username = 'cheruden';
+  }
+  getProfileInformation() {
+    return this.http.get(`https://api.github.com/users/${this.username}?access_token=${environment.apiKey}`)
+  }
+  getProfileRepos() {
+    return this.http.get(`https://api.github.com/users/${this.username}/repos?access_token=${environment.apiKey}`)
+  }
+  updateUserProfile(username: string) {
+    this.username = username;
+  }
+}
