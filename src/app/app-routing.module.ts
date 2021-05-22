c5ebb897dd54fe44872abfc4245dabb3f88cfbcf
cheruden <cheruden25@gmail.com>
2021-05-22 13:48:48 +0300

    add links to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'git', component: GithubComponent },
+  { path: 'about', component: AboutComponent },
 
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
